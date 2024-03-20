518abf4fd4fd9cd01e41180a89645e9e9c6e111b
aniket <aniket.umbc@gmail.com>
2024-03-20 09:50:47 +0530

    added log

--- weather.js
+++ weather.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
  */
 export async function getWeather(city) {
   const apiKey = process.env.WEAHER_API_KEY
+  console.log(process.env.WEAHER_API_KEY)
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
   const response = await fetch(url)
